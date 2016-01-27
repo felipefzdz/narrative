d86a450db4b8bf80c3478c72d3205f155c71860d
Felipe Fernandez <olid16@gmail.com>
2016-01-27 20:50:51 +0000

    Add slf4j-api and simple binding just to prove that logging works in test scope.

--- narrative-scalatest/src/test/scala/com/narrative/NarrativeSpec.scala
+++ narrative-scalatest/src/test/scala/com/narrative/NarrativeSpec.scala
@@ -3,14 +3,17 @@ package com.narrative
 import org.junit.runner.RunWith
 import org.scalatest.WordSpec
 import org.scalatest.junit.JUnitRunner
+import org.slf4j.LoggerFactory
 
 @RunWith(classOf[JUnitRunner])
 class NarrativeSpec extends WordSpec with Narrative {
 
+  val log: org.slf4j.Logger = LoggerFactory.getLogger(classOf[NarrativeSpec])
   implicit val logger: Logger = new Logger()
 
   "Narrative" should {
     "pass when the expected lines are logged" in {
+      log.info("Hello from the logs!")
       new SystemUnderTest().triggerGreetings()
       loggingNarrativeShouldBe(
         "Hi",
